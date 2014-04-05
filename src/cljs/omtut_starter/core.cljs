3a8ab36f26e02895396192f0c2fae7d5913f18d3
Josh Lehman <jalehman37@gmail.com>
2014-04-05 12:40:08 -0700

    update deps

--- src/cljs/omtut_starter/core.cljs
+++ src/cljs/omtut_starter/core.cljs
@@ -35,14 +35,14 @@
   (om/component
    (let [raw-markup (md/mdToHtml text)]
      (dom/div #js {:className "comment"}
-              (dom/h2 #js {:className "commentAuthor"} author)
-              (dom/span #js {:dangerouslySetInnerHTML #js {:__html raw-markup}})))))
+       (dom/h2 #js {:className "commentAuthor"} author)
+       (dom/span #js {:dangerouslySetInnerHTML #js {:__html raw-markup}})))))
 
 (defn comment-list [{:keys [comments]}]
   (om/component
-   (dom/div #js {:className "commentList"}
-            (om/build-all comment comments
-                          {:key :id}))))
+   (apply dom/div #js {:className "commentList"}
+          (om/build-all comment comments
+            {:key :id}))))
 
 (defn save-comment!
   [comment url]
@@ -57,7 +57,7 @@
       (let [comment {:author author :text text}]
         (save-comment! comment (:url opts))
         (om/transact! app [:comments]
-                    (fn [comments] (conj comments (assoc comment :id (guid))))))
+                      (fn [comments] (conj comments (assoc comment :id (guid))))))
       (clear-nodes! author-node text-node))
     false))
 
@@ -66,10 +66,10 @@
     om/IRender
     (render [_]
       (dom/form
-       #js {:className "commentForm" :onSubmit #(handle-submit % owner app opts)}
-       (dom/input #js {:type "text" :placeholder "Your Name" :ref "author"})
-       (dom/input #js {:type "text" :placeholder "Say something..." :ref "text"})
-       (dom/input #js {:type "submit" :value "Post"})))))
+        #js {:className "commentForm" :onSubmit #(handle-submit % owner app opts)}
+        (dom/input #js {:type "text" :placeholder "Your Name" :ref "author"})
+        (dom/input #js {:type "text" :placeholder "Say something..." :ref "text"})
+        (dom/input #js {:type "submit" :value "Post"})))))
 
 (defn fetch-comments
   [url]
@@ -92,9 +92,9 @@
     om/IRender
     (render [_]
       (dom/div #js {:className "commentBox"}
-               (dom/h1 nil "Comments")
-               (om/build comment-list app)
-               (om/build comment-form app {:opts opts})))))
+        (dom/h1 nil "Comments")
+        (om/build comment-list app)
+        (om/build comment-form app {:opts opts})))))
 
 (defn omtut-starter-app [app owner]
   (reify
@@ -102,8 +102,8 @@
     (render [_]
       (dom/div nil
         (om/build comment-box app
-                  {:opts {:url "/comments"
-                          :poll-interval 2000}})))))
+          {:opts {:url "/comments"
+                  :poll-interval 2000}})))))
 
 
-(om/root app-state omtut-starter-app (.getElementById js/document "content"))
+(om/root omtut-starter-app app-state {:target (.getElementById js/document "content")})
